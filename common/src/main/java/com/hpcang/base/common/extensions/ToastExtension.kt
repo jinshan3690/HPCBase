767ade8609efe8527288159716665bcee3deba15
Js <190261036@qq.com>
2020-06-29 09:33:40 +0800

    第一次提交

--- common/src/main/java/com/hpcang/base/common/extensions/ToastExtension.kt
+++ common/src/main/java/com/hpcang/base/common/extensions/ToastExtension.kt
@@ -10,7 +10,7 @@ var text :Int = 0
  * 短时间显示Toast
  */
 @JvmOverloads
-fun showToast(gravity: Int = Gravity.CENTER, duration: Int = Toast.LENGTH_SHORT, message: () -> String) {
+fun showToast(gravity: Int = Gravity.CENTER, duration: Int = Toast.LENGTH_SHORT, message: () -> String?) {
     val toast = Toast.makeText(BaseApplication.getContext(), message(), duration)
     toast.setGravity(gravity, 0, 0)
     toast.show()
@@ -20,7 +20,7 @@ fun showToast(gravity: Int = Gravity.CENTER, duration: Int = Toast.LENGTH_SHORT,
  * 长时间显示Toast
  */
 inline fun showToastLong(
-    gravity: Int = Gravity.CENTER, duration: Int = Toast.LENGTH_LONG, message: () -> String
+    gravity: Int = Gravity.CENTER, duration: Int = Toast.LENGTH_LONG, message: () -> String?
 ) {
     val toast = Toast.makeText(BaseApplication.getContext(), message(), duration)
     toast.setGravity(gravity, 0, 0)
